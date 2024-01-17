1c0edc079ff910b6716cd6c86378adbca323d57d
Maks-Hunzyuk <115732268+Maks-Hunzyuk@users.noreply.github.com>
2024-01-17 11:24:32 +0300

    17.01.24

---
+++ goods/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+def catalog(request):
+    context = {
+        "title": "Home - Каталог",
+    }
+    return render(request, "goods/catalog.html")
+
+
+def product(request):
+    return render(request, "goods/product.html")
