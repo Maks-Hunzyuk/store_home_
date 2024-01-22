6387537e9d17f72cda4cef64562c45bc185986ba
Maks-Hunzyuk <115732268+Maks-Hunzyuk@users.noreply.github.com>
2024-01-22 12:34:19 +0300

    22.01.24 All cart templates were included

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from users.views import login, registration, profile, logout
+from users.views import login, registration, profile, logout, users_cart
 
 app_name = 'users'
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path("login/", login, name='login'),
     path("registration/", registration, name='registration'),
     path("profile/", profile, name="profile"),
-    path("logout/", logout, name="logout")
+    path("logout/", logout, name="logout"),
+    path("users-cart/", users_cart, name="users-cart"),
 ]
