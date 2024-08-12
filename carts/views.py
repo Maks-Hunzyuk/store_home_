9c6f51aa8aa6f70a9991b0e91c1c1cb619833fdb
Maks-Hunzyuk <115732268+Maks-Hunzyuk@users.noreply.github.com>
2024-08-12 16:33:15 +0300

    Добавил функционал для удаления товаров из карзины

--- carts/views.py
+++ carts/views.py
@@ -9,6 +9,7 @@ from carts.models import Cart
 
 
 def cart_add(request, product_slug):
+    """Добавление товара в карзину"""
     product = Products.objects.get(slug=product_slug)
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, product=product)
@@ -26,6 +27,8 @@ def cart_change(request, product_slug):
     ...
 
 
-def cart_remove(request, product_slug):
+def cart_remove(request, cart_id):
     """Удаление карты товара из карзины"""
-    ...
+    cart = Cart.objects.get(id=cart_id)
+    cart.delete()
+    return redirect(request.META["HTTP_REFERER"])
